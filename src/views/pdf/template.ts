d0c4afa23ee223c3ecd877422914bb42ad0be5c1
Radoan Hossan <radoanhossaiinpream@outlook.com>
2024-07-04 23:10:32 +0600

    Footer and specing fixing with unnecessary file deletion

--- src/views/pdf/template.ts
+++ src/views/pdf/template.ts
@@ -31,7 +31,7 @@ const template = function (data: any) : string {
             </p>
         </td>
         <td>
-            <p style="display: flex;align-items: center;">
+            <p style="display: flex;align-items: center;text-transform: capitalize;">
                 ${topArrowIcon}
                 ${keyword.beforeSeoRanking} <span style="color: #39B54A;font-size: 10px;margin-left: 10px;">${keyword.beforeSeoRankingChange}</span>
             </p>
@@ -68,7 +68,7 @@ const template = function (data: any) : string {
             font-size: 12px;
         }
         td{
-            padding: 15px 20px;
+            padding: 10px 20px;
             text-align: center;
         }
         tr:last-child td:first-child{
@@ -160,12 +160,15 @@ const template = function (data: any) : string {
     </div>
     <div style="display: flex;">
         <div style="width: 10%">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve">
-            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
-                <path d="M 45 0 c 15.103 0 27.389 12.287 27.389 27.389 C 72.389 46.616 46.147 66.607 45 90 c -1.147 -23.393 -27.389 -43.384 -27.389 -62.611 C 17.611 12.287 29.897 0 45 0 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                <circle cx="45.004999999999995" cy="26.575000000000003" r="9.205" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(45,41,115); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
-            </g>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve">
+    <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+    <path d="M 81.517 86.864 H 60.004 c -1.519 0 -2.75 -1.231 -2.75 -2.75 V 23.222 c 0 -1.519 1.231 -2.75 2.75 -2.75 h 21.513 c 1.519 0 2.75 1.231 2.75 2.75 v 60.892 C 84.267 85.633 83.036 86.864 81.517 86.864 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+        <path d="M 64.439 25.891 h 23.017 c 2.262 0 3.395 -2.858 1.795 -4.53 L 72.556 3.913 c -0.991 -1.036 -2.599 -1.036 -3.59 0 L 52.27 21.361 c -1.599 1.672 -0.467 4.53 1.795 4.53 L 64.439 25.891" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+        <path d="M 52.408 84.306 V 32.767 H 38.867 c -1.413 0 -2.558 1.145 -2.558 2.558 v 48.981 c 0 1.413 1.145 2.558 2.558 2.558 h 16.099 C 53.553 86.864 52.408 85.719 52.408 84.306 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+        <path d="M 31.463 84.306 V 45.062 h -10.75 c -1.413 0 -2.558 1.145 -2.558 2.558 v 36.686 c 0 1.413 1.145 2.558 2.558 2.558 h 13.308 C 32.608 86.864 31.463 85.719 31.463 84.306 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+        <path d="M 13.308 84.306 V 57.357 H 2.558 C 1.145 57.357 0 58.502 0 59.915 v 24.391 c 0 1.413 1.145 2.558 2.558 2.558 h 13.308 C 14.454 86.864 13.308 85.719 13.308 84.306 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+</g>
+</svg>
         </div>
         <p style="width: 90%;margin-top: 0;color: #ffffff; word-spacing: 2px;">
             Represents the website rankings in the knowledge panel of google search results.
@@ -175,12 +178,11 @@ const template = function (data: any) : string {
     </div>
     <div style="display: flex;">
         <div style="width: 10%">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve">
-            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
-                <path d="M 45 0 c 15.103 0 27.389 12.287 27.389 27.389 C 72.389 46.616 46.147 66.607 45 90 c -1.147 -23.393 -27.389 -43.384 -27.389 -62.611 C 17.611 12.287 29.897 0 45 0 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                <circle cx="45.004999999999995" cy="26.575000000000003" r="9.205" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(45,41,115); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
-            </g>
-        </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve">
+              <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+                  <path d="M 85.414 11.8 H 4.586 C 2.053 11.8 0 13.853 0 16.386 v 57.228 C 0 76.147 2.053 78.2 4.586 78.2 h 80.828 c 2.533 0 4.586 -2.053 4.586 -4.586 V 16.386 C 90 13.853 87.947 11.8 85.414 11.8 z M 36.475 25.917 c 3.922 0 7.101 3.179 7.101 7.101 c 0 3.922 -3.179 7.101 -7.101 7.101 c -3.922 0 -7.101 -3.179 -7.101 -7.101 C 29.375 29.096 32.554 25.917 36.475 25.917 z M 81.269 73.163 H 8.731 c -2.273 0 -4.116 -1.843 -4.116 -4.116 l 15.26 -18.195 c 2.753 -3.282 7.745 -3.448 10.71 -0.355 l 0.963 1.005 c 2.991 3.12 8.038 2.919 10.771 -0.43 l 7.714 -9.451 c 2.809 -3.441 8.034 -3.542 10.973 -0.21 l 24.38 27.637 C 85.385 71.32 83.542 73.163 81.269 73.163 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+              </g>
+          </svg>
         </div>
         <p style="width: 90%;margin-top: 0;color: #ffffff; word-spacing: 2px;">
             Represents the website rankings in the carousel section of google search results.
@@ -197,14 +199,17 @@ const template = function (data: any) : string {
             toolbar: {
                 show: false
             },
-            height: 200,
+            height: 220,
         },
         series: [{
             name: 'Ranking',
             data: dynamicData.rankings
         }],
         grid: {
-            show: false
+            show: true,
+            borderColor: '#26225F',
+            strokeDashArray: 0,
+            position: 'back',
         },
         xaxis: {
             categories: dynamicData.xAxisData,
